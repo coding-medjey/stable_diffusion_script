5c30da215a6d48de4ed0014f798306504f89744b
Santhanam <154440698+Santta1@users.noreply.github.com>
2024-03-22 14:27:47 +0530

    Add commit

---
+++ configfile.py
@@ -0,0 +1,4 @@
+# Define Cloudflare configuration directly in the script
+CLOUDFLARE_ENDPONT_URL: str = 'https://36be2b889dd21fc22f59d8342ea695fe.r2.cloudflarestorage.com/photostudio'
+CLOUDFLARE_AWS_ACCESS_KEY_ID: str = "d1ecd13dd419cb2c1e9bcda171389f4a"
+CLOUDFLARE_AWS_SECRET_ACCESS_KEY:str = "5b244c4f1e4620ab968fda2431ec97f78568ba5abf6fcf10191fa01232e6e88c"
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import boto3
 from datetime import datetime
 import secrets
 from io import BytesIO  # Import BytesIO
+import configfile
 
 
 def read_input():
@@ -52,8 +53,8 @@ def list_object(prefix):
         s3_client = boto3.client(
             service_name='s3',
             endpoint_url="https://36be2b889dd21fc22f59d8342ea695fe.r2.cloudflarestorage.com/",
-            aws_access_key_id=CLOUDFLARE_AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=CLOUDFLARE_AWS_SECRET_ACCESS_KEY,
+            aws_access_key_id=configfile.CLOUDFLARE_AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=configfile.CLOUDFLARE_AWS_SECRET_ACCESS_KEY,
             
         )
         res = s3_client.list_objects(Bucket="photostudio", Prefix=prefix)
