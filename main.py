82acac629c1cc77a069995c87bfad2d7ce1ae6af
Bhirahateesvaran Periyasamy <71700975+coding-medjey@users.noreply.github.com>
2024-04-10 16:35:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,10 +53,9 @@ def generate_image_from_prompt():
     # Upload image to Cloudflare
     job_id = uploadfile(images,request.job_id)
     if job_id:
-          print(f"https://pub-b98e7fd0839f42c4bb6c36c680b13023.r2.dev/{job_id}/0")
-          
-    else: 
-          return False
+          print(f"https://pub-b98e7fd0839f42c4bb6c36c680b13023.r2.dev/{job_id}/0")    
+    else:
+        print("Failed to upload image to Cloudflare.")
         
 
 def list_object(prefix):
